55883cd7b4bc1ce9c989344f510745bc333d1c10
cagriyildiz <cag.yild@gmail.com>
2018-01-19 23:14:59 +0300

    fixed indentation

--- src/notification.py
+++ src/notification.py
@@ -13,22 +13,23 @@ ONE_DAY = 86400
 
 
 def get_previous_notification_order(driver):
-	element = driver.find_elements_by_css_selector('#noDisclosuresToShow:not(.ng-hide)')
-	previous_notifications_present = len(element) == 0
-	if (previous_notifications_present):
-		notification_order = driver.find_element_by_css_selector('.notifications-row > a').text
-	else
-		logging.info('LOG: no previous notification found')
-		notification_order = 0
-	logging.info('LOG: previous notification number: ', notification_order)
-	return notification_order
+    element = driver.find_elements_by_css_selector('#noDisclosuresToShow:not(.ng-hide)')
+    previous_notifications_present = len(element) == 0
+    if previous_notifications_present:
+        notification_order = driver.find_element_by_css_selector('.notifications-row > a').text
+    else:
+        logging.info('LOG: no previous notification found')
+        notification_order = 0
+
+    logging.info('LOG: previous notification number: ', notification_order)
+    return notification_order
 
 
 def get_new_notifications(driver, previous_notification_number):
-	notification_list = driver.find_elements_by_tag_name('disclosure-list-item')
-	uppermost_notification_number = notification_list[0].get_attribute('item-order')
-	len = uppermost_notification_number - previous_notification_number
-	return notification_list[0:len]
+    notification_list = driver.find_elements_by_tag_name('disclosure-list-item')
+    uppermost_notification_number = notification_list[0].get_attribute('item-order')
+    length = uppermost_notification_number - previous_notification_number
+    return notification_list[0:length]
 
 
 def print_notification(notification_number, notification_url, company_code):
@@ -43,14 +44,16 @@ def listen_notifications(driver):
         new_notification = WebDriverWait(driver, ONE_DAY).until(
             ec.visibility_of_element_located((By.CLASS_NAME, 'newNotifications'))
         )
-		previous_notification_order = get_previous_notification_order(driver)
-		ActionChains(driver).click(new_notification).perform()
-		#sleep(2)
+        previous_notification_order = get_previous_notification_order(driver)
+        ActionChains(driver).click(new_notification).perform()
+        # sleep(2)
         notifications = get_new_notifications(driver, previous_notification_order)
         for notification in reversed(notifications):
-			notification_id = notification.find_element_by_class_name('_12').get_attribute('href')
-			stock_code = notification.find_element_by_css_selector('._3 span').get_attribute('title')
-			print_notification(notification_order, notification_id, stock_code)
+            notification_order = notification[0].get_attribute('item-order')
+            notification_id = notification.find_element_by_class_name('_12').get_attribute('href')
+            stock_code = notification.find_element_by_css_selector('._3 span').get_attribute('title')
+            print_notification(notification_order, notification_id, stock_code)
+
     except TimeoutException:
         logging.info('LOG: no new notification found in 24 hours')
     finally:
